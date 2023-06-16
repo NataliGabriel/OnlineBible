a1f17821cd8c14088d678462525c394956eb5beb
NataliGabriel <bielnatali7@gmail.com>
2023-06-16 16:57:27 -0300

    commitando

---
+++ Data/BibleAPIService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace APIBible.Data
+{
+    public class BibleAPIService
+    {
+        public const string BaseUrl = "https://www.abibliadigital.com.br/api/";
+        private readonly HttpClient _httpClient;
+
+
+        public BibleAPIService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(BaseUrl);
+        }
+
+        public BibleModel[] GetBooks()
+        {
+            string GetBooksURL = BaseUrl + "books";
+            var response = _httpClient.GetStringAsync(GetBooksURL).Result;
+            return JsonConvert.DeserializeObject<BibleModel[]>(response);
+        }
+        public FindBooks GetChapter(string abbrev)
+        {
+            string GetBooksURL = BaseUrl + "books/" + abbrev;
+            var response = _httpClient.GetStringAsync(GetBooksURL).Result;
+            return JsonConvert.DeserializeObject<FindBooks>(response);
+        }
+        public Scripture GetScripture(string abbrev, string chapter)
+        {
+            string GetBooksURL = BaseUrl + "verses/acf/" + abbrev +"/" + chapter;
+            var response = _httpClient.GetStringAsync(GetBooksURL).Result;
+            return JsonConvert.DeserializeObject<Scripture>(response);
+        }
+    }
+}
---
+++ Data/BibleModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json.Serialization;
+
+namespace APIBible.Data
+{
+    public class ResponseData
+    {
+        public BibleModel[] bible { get; set; }
+    }
+    public class BibleModel
+    {
+        public string name { get; set; }
+        public string chapters { get; set; }
+        public abbrev abbrev { get; set; }
+
+    }
+    public class abbrev
+    {
+        public string pt { get; set; }
+        public string en { get; set; }
+    }
+    public class FindBooks
+    {
+        public abbrev abbrev { get; set; }
+        public string Author { get; set; }
+        public int Chapters { get; set; }
+        public string Comment { get; set; }
+        public string Group { get; set; }
+        public string Name { get; set; }
+        public string Testament { get; set; }
+    }
+    public class FindVerses
+    {
+        public abbrev Abbrev { get; set; }
+        public string Name { set; get; }
+        public string Author { set; get; }
+        public string Group { get; set; }
+        public string Version { get; set; }
+    }
+    public class Chapter
+    {
+        public int Number { get; set; }
+        public int Verses { get; set; }
+    }
+    public class Verse
+    {
+        public int Number { get; set; }
+        public string Text { get; set; }
+    }
+    public class Scripture
+    {
+        public FindVerses Book { get; set; }
+        public Chapter Chapter { get; set; }
+        public Verse[] verses { get; set; }
+    }
+}
